bba7d108ec653d3f3cabe133aad660a73b9355a1
youssefadel94 <darkmetalxdragon@hotmail.com>
2016-07-14 15:54:08 +0200

    adding funtional

--- Student_Course_Demo/scripts/controller/MainControllers.js
+++ Student_Course_Demo/scripts/controller/MainControllers.js
@@ -2,69 +2,82 @@ app.controller("MainController", function ($scope, $http/*, ApiCall*/) {
 
     $http.get("/api/Cours")
          .then(function (response) {
-               $scope.courses = response.data;
-           
+             $scope.courses = response.data;
+
          });
     $http.get("/api/Students")
         .then(function (response) {
             $scope.students = response.data;
 
         });
+    var Refresh = function(Des){
+        if (Des == "Students") {
+            $http.get("/api/Students")
+   .then(function (response) {
+       $scope.students = response.data;
 
-    $scope.delete = function (name,id) {
-        $http.delete("/api/"+name+"/" + id).success(function (data) {
+   });
+        }
+        else {
+            if (Des == "Cours") {
+                $http.get("/api/Cours")
+     .then(function (response) {
+         $scope.courses = response.data;
+
+     });
+            }
+        }
+    }
+    $scope.delete = function (name, id) {
+        $http.delete("/api/" + name + "/" + id).success(function (data) {
             //either this
             //$scope.refresh();
             //or this "how to refresh"
             //$scope.students.splice(id, 1);
-            if (name == "Students") {
-                $http.get("/api/Students")
-       .then(function (response) {
-           $scope.students = response.data;
-
-       });
-            }
-            else {
-                if (name == "Cours") {
-                    $http.get("/api/Cours")
-         .then(function (response) {
-             $scope.courses = response.data;
-
-         });
-                }
-            }
-            });
-    };
-  /*  $scope.create = function (name, data) {
-        $http.post("/api/" + name + "/", data).success(function (dataR) {
-            //either this
-            $scope.refresh();
-            //or this "how to refresh"
-            //$scope.students.splice(id, 1);
+            Refresh(name);
         });
-    };*/
+    };
+    /*  $scope.create = function (name, data) {
+          $http.post("/api/" + name + "/", data).success(function (dataR) {
+              //either this
+              $scope.refresh();
+              //or this "how to refresh"
+              //$scope.students.splice(id, 1);
+          });
+      };*/
 
-    $scope.Name = '';
+    $scope.CName = '';
+    $scope.SName = '';
     $scope.Level = '';
     $scope.Gender = '';
     $scope.Age = '';
     $scope.Year = '';
 
     $scope.submit = function (Des) {
-        if ($scope.Name && $scope.Level) {
-            Date = [{ Name: $scope.Name, Level: $scope.Level }];
+        if (Des == "Cours") {
+            if ($scope.CName && $scope.Level) {
+                var Data = { "Name": $scope.CName, "Level": $scope.Level };
+                var jData = JSON.stringify(Data);
+                $http.post("/api/" + Des, jData, []).success(function (DataR) {
+                    Refresh(Des);
+                });
 
-            $http.post("/api/" + Des , Data).success(function (DataR) {
-                $http.get("/api/Cours")
-          .then(function (response) {
-              $scope.courses = response.data;
+            }
+        }
+        else {
+            if (Des == "Students") {
+                if ($scope.SName && $scope.Gender && $scope.Year && $scope.Age) {
+                    var Data = { "Name": $scope.SName, "Gender": $scope.Gender, "Age": $scope.Age, "Year": $scope.Year };
+                    var jData = JSON.stringify(Data);
+                    $http.post("/api/" + Des, jData, []).success(function (DataR) {
+                        Refresh(Des);
+                    });
 
-          });
-            });
-
+                }
+            }
         }
     }
-    
+
 });
 /*/ Simple GET request example:
 $http({
