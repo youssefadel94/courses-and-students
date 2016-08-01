216bb8da877b34560c713d4b44f3ed8e1701fd71
youssefadel94 <darkmetalxdragon@hotmail.com>
2016-08-01 19:48:25 +0200

    bussines layer 0.2

---
+++ Student.BusinessLogic/Managers/CoursesManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using System.Web.Http;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+
+
+
+namespace BusinessLogic.Managers
+{
+    class CoursesManager
+    {
+
+        private StudentEntities db = new StudentEntities() ;
+      
+        public IQueryable<Cours> GetCourses()
+        {
+            return db.Courses ;
+        }
+
+
+        public async Task<Cours> GetCours(int id)
+        {
+            Cours cours = await db.Courses.FindAsync(id);
+            
+
+            return cours;
+        }
+
+
+    }
+}
